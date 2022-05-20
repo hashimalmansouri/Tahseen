14c0241e597f4ba6efc6039d4dc84112ef60ed95
Hashim Al-Mansouri <hashimalmansouri@outlook.com>
2022-05-20 16:46:03 +0300

    scripts

---
+++ Tahseen/Scripts/site.js
@@ -0,0 +1,11 @@
+﻿const home = document.querySelector('.bars');
+const navbar = document.querySelector('.navbar ul');
+
+
+function showNavbar() {
+	home.addEventListener('click', () => {
+		navbar.classList.toggle('fix-top');
+
+	});
+}
+showNavbar();
