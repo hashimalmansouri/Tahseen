3809b4130ed0e8a3044b050406c49f9c2b971741
Hashim Al-Mansouri <hashimalmansouri@outlook.com>
2022-05-31 23:25:13 +0300

    .

--- Tahseen/App_Start/IdentityConfig.cs
+++ Tahseen/App_Start/IdentityConfig.cs
@@ -81,10 +81,10 @@ namespace Tahseen
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
